34273a7613bce4953212fc480cfe21bedc493224
quyifan <yvan.qu@mail.utoronto.ca>
2022-03-02 01:06:16 -0500

    all

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const path = require('path')
 
 const app = express()
 
-app.get("*",(req, res) => {
-    app.use(express.static("build"))
-    console.log("get /")
+// app.get("*",(req, res) => {
+//     app.use(express.static("build"))
+//     console.log("get /")
     
-    res.sendFile(path.join(__dirname, 'build','index.html'))
+//     res.sendFile(path.join(__dherirname, 'build','index.html'))
+// })
+
+const root = require('path').join(__dirname, 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
 })
 
 const port = process.env.PORT || 3000
